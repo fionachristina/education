706e0adc363fa36a7c8e89e3f747edfd2694a91e
fionachristina <38747291+fionachristina@users.noreply.github.com>
2018-10-24 14:54:51 +0300

    new DB

--- scholarly.sql
+++ scholarly.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 15, 2018 at 06:59 AM
--- Server version: 10.1.32-MariaDB
--- PHP Version: 7.2.5
+-- Generation Time: Oct 24, 2018 at 01:48 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `examplee`
+-- Database: `scholarly`
 --
 
 -- --------------------------------------------------------
@@ -45,6 +45,33 @@ INSERT INTO `admins` (`username`, `user_type`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `faculty`
+--
+
+CREATE TABLE `faculty` (
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `subject` varchar(50) NOT NULL,
+  `location` varchar(50) NOT NULL,
+  `image` varchar(5000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subject`
+--
+
+CREATE TABLE `subject` (
+  `name` varchar(50) NOT NULL,
+  `book _cover` varchar(5000) NOT NULL,
+  `link` varchar(5000) NOT NULL,
+  `video` varchar(5000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -63,7 +90,8 @@ INSERT INTO `users` (`id`, `username`, `password`, `created_at`) VALUES
 (4, 'Mbiraru', '$2y$10$Gn3Mg5KUr/B62hpTdASIe.EMk.SOLRZCDQFrJvvaKNejuoyvrfNrq', '2018-06-27 16:14:15'),
 (5, 'Juliet', '$2y$10$VSIJwiauOHlQvaYEEZ03KeljuyzDFXjcrG7uZB27O/GXzhgP0PLgK', '2018-07-09 14:30:11'),
 (6, 'Kamau', '$2y$10$nbRVaFyvXJxv5QFMOPpQaus0GA4FkwqBBMzeiCdn8Cop4LVyjpe9u', '2018-10-15 05:14:53'),
-(7, 'Nyaga', '$2y$10$vVBJZLQzKVlyduNZpxyPt.uieiG9xz9lfMmYjHy9TnyvZArbzbTAu', '2018-10-15 07:34:25');
+(7, 'Nyaga', '$2y$10$vVBJZLQzKVlyduNZpxyPt.uieiG9xz9lfMmYjHy9TnyvZArbzbTAu', '2018-10-15 07:34:25'),
+(8, 'Lincoln Kamau', '$2y$10$i51a/0xDv5dCKJnu/QIAUuq6B8T0KMob6fVZj7VL8saq7myT7nC1.', '2018-10-24 12:29:44');
 
 --
 -- Indexes for dumped tables
@@ -84,7 +112,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
